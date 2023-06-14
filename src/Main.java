d1bd66afde4b7169348a7d89f1a6fd7c3a913d86
Ілля <Atamasillyaandriyovich@gmail.com>
2023-06-15 00:40:27 +0300

    Багатопоточність

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,33 @@
 import java.time.Duration;
 import java.time.Instant;
-
-
-
 public class Main {
     public static void main(String[] args) {
-        TimerThread timerThread = new TimerThread();
+        Thread timerThread = new Thread(() -> {
+            Instant start = Instant.now();
+            while (true) {
+                Instant current = Instant.now();
+                Duration duration = Duration.between(start, current);
+                System.out.println("Пройшло " + duration.getSeconds() + " сек.");
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+
+        Thread messageThread = new Thread(() -> {
+            while (true) {
+                try {
+                    System.out.println("Минуло 5 секунд");
+                    Thread.sleep(5000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+
         timerThread.start();
+        messageThread.start();
     }
 }
